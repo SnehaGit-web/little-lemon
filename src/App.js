5f75c19204a58ea460e71efcf08c59fc77709620
Sneha Ann George <snehaanngeorge.ca@gmail.com>
2026-05-10 11:27:50 -0400

    Add GitHub Pages deployment configuration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './App.css';
 import './styles.css';
 import Nav from './components/Nav';
